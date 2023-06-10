28b998f5a3cdfc8fc2a9b8e892d2231261d2d784
uncle-gua <scnb2681@163.com>
2023-06-10 20:31:23 +0800

    initial commit

--- forms/customer/save.go
+++ forms/customer/save.go
@@ -5,9 +5,9 @@ type Save struct {
 	Exchange  string  `form:"exchange" binding:"required,oneof=BINANCE OKX"`
 	ApiKey    string  `form:"apiKey" binding:"required"`
 	ApiSecret string  `form:"apiSecret" binding:"required"`
-	Capital   float64 `form:"capital" binding:"required"`
-	Scale     float64 `form:"scale" binding:"required"`
-	Level1    float64 `form:"level1" binding:"required"`
-	Level2    float64 `form:"level2" binding:"required"`
+	Capital   float64 `form:"capital" binding:"gte=0"`
+	Scale     float64 `form:"scale" binding:"gt=0"`
+	Level1    float64 `form:"level1" binding:"gt=0"`
+	Level2    float64 `form:"level2" binding:"gt=0"`
 	Status    string  `form:"status" binding:"required,oneof=Enable Disable"`
 }
